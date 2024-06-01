6496b3a703b9b1ca48290d25c65cae9552fae310
alang2211 <pslesarev91@gmail.com>
2024-06-01 02:31:13 +0200

    Add Sphinx configuration and Read the Docs configuration

---
+++ docs/conf.py
@@ -0,0 +1,21 @@
+# docs/conf.py
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'Cryptotrade'
+author = 'Your Name'
+release = '0.1'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
